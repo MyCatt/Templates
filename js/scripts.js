31d456d3a4db33ae83e7cc7f0e74dd827b693f5c
MyCatt <43209825+MyCatt@users.noreply.github.com>
2018-09-13 23:18:05 +1200

    Added Javascript via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var hamburger = document.getElementById("hamburger");
+var primary_navigation = document.getElementById("primary-navigation");
+hamburger.addEventListener("click", function(){
+    if(primary_navigation.classList.contains("active-nav")) {
+    	primary_navigation.classList.remove("active-nav");
+    	primary_navigation.classList.add("deactive-nav");
+    }else{
+    	primary_navigation.classList.add("active-nav");
+    	primary_navigation.classList.remove("deactive-nav");
+    }
+});
